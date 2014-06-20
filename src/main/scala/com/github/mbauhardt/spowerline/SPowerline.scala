3b7c7d6c5e2e51d02d3f896690b35f624d11afae
Marko Bauhardt <marko.bauhardt@googlemail.com>
2014-06-20 14:37:01 +0200

    Update SPowerline.scala

--- src/main/scala/com/github/mbauhardt/spowerline/SPowerline.scala
+++ src/main/scala/com/github/mbauhardt/spowerline/SPowerline.scala
@@ -2,24 +2,42 @@ package com.github.powerline
 
 import java.net.InetAddress
 import scala.sys.process._
+import java.io.File
 
 object SPowerline extends App {
+
+  def source = "source ~/.zshrc"
+
+  def insideWorkingTree = "git rev-parse --is-inside-work-tree"
+
+  def gitInfoProvidedByOhMyZsh = "git_prompt_info"
+
+  def combineProcessArguments(arguments: Seq[String]): String = arguments.mkString(" && ")
+
   def userFunction: String = System.getProperties().get("user.name").asInstanceOf[String]
+
   def hostFunction: String = "@" + InetAddress.getLocalHost().getHostName()
+
   def pwdFunction: String = "pwd".!!.replace("\n", "")
-  //def branchFunction: String = "${$(git symbolic-ref HEAD 2> /dev/null || git rev-parse --short HEAD 2> /dev/null)#refs/heads/}".!!.replace("\n", "")
+
   def branchFunction: String = {
-    val branchName = "git symbolic-ref HEAD".!!.replace("\n", "")
-    val commitId  = "git rev-parse --short HEAD".!!.replace("\n", "")
-    s"$branchName($commitId)"
+
+    def isGitRepository: Boolean = Process(Seq("zsh", "-c", combineProcessArguments(Seq(source, insideWorkingTree)))).! == 0
+
+    if (isGitRepository) {
+      Process(Seq("zsh", "-c", combineProcessArguments(Seq(source, gitInfoProvidedByOhMyZsh)))).!!
+    } else {
+      ""
+    }
+  }
+
+  def render(powerline: Powerline) {
+    println(powerline.segments.map(segment => segment.value).mkString(" > "))
   }
 
   val powerline = new Powerline(Seq(new Segment(userFunction), new Segment(hostFunction), new Segment(pwdFunction), new Segment(branchFunction)))
   render(powerline)
 
-  def render(powerline: Powerline) {
-    println(powerline.segments.map(segment => segment.value).mkString(" > "))
-  }
 }
 
 class Powerline(val segments: Seq[Segment])
