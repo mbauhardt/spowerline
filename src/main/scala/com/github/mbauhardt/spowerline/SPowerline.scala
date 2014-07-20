7568191d132f9139405f7b2df5ffa31b9f943b9d
Marko Bauhardt <marko.bauhardt@googlemail.com>
2014-07-20 22:45:07 +0200

    show last exit status only if failedin red

--- src/main/scala/com/github/mbauhardt/spowerline/SPowerline.scala
+++ src/main/scala/com/github/mbauhardt/spowerline/SPowerline.scala
@@ -17,42 +17,22 @@ object Util {
 
 
 object Common {
-  lazy val segmentSeparatorContent = Util.execute(Seq("echo -e \"\\xE2\\xAE\\x80\""))
-  lazy val emptySegment: Segment = Segment("")
-  lazy val lastExitStatusSegment: Segment = Segment("%?", "black", "green", SegmentSeparator(segmentSeparatorContent, "green", "blue"))
-  lazy val timeSegment: Segment = Segment("%D{%a %d-%b}%@", "black", "blue", SegmentSeparator(segmentSeparatorContent, "blue", "white"))
-  lazy val hostSegment: Segment = Segment("%n@%M", "black", "white", SegmentSeparator(segmentSeparatorContent, "white", "cyan"))
-  lazy val pwdSegment: Segment = Segment("%~", "black", "blue", SegmentSeparator(segmentSeparatorContent, "blue", "black"))
-}
-
-object Git {
-  //  val insideWorkingTree: Command = "git rev-parse --is-inside-work-tree"
-  //  val gitInfo: Command = "git_prompt_info"
-  //  val gitInfoProvidedByOhMyZsh: Segment = () =>
-  //  def branchFunction: Segment = {
-
-  //def isGitRepository: Boolean = Process(Seq("zsh", "-c", Util.combineCommands(Seq(Util.source, insideWorkingTree)).apply())).! == 0
-
-  //    if (isGitRepository) {
-  //      () => Process(Seq("zsh", "-c", Util.combineCommands(Seq(Util.source, gitInfoProvidedByOhMyZsh)).apply())).!!
-  //    } else {
-  //      Common.empty
-  //    }
-  //  }
+  val segmentSeparatorContent = Util.execute(Seq("echo -e \"\\xE2\\xAE\\x80\""))
+  val emptySegment: Segment = Segment("")
+  val lastExitStatusSegment: Segment = Segment("%(?..%?)", "red", "blue", SegmentSeparator(segmentSeparatorContent, "blue", "blue"))
+  val timeSegment: Segment = Segment("%D{%a %d-%b}%@", "black", "blue", SegmentSeparator(segmentSeparatorContent, "blue", "white"))
+  val hostSegment: Segment = Segment("%n@%M", "black", "white", SegmentSeparator(segmentSeparatorContent, "white", "cyan"))
+  val pwdSegment: Segment = Segment("%~", "black", "blue", SegmentSeparator(segmentSeparatorContent, "blue", "black"))
 }
 
 object SPowerline extends App {
+
+
   def renderPowerline(powerline: Powerline) = {
     println("\r\n" + powerline.segments.map(s =>
       //
-      "%{$bg[" + s.bgColor + "]%}"
-        + "%{$fg[" + s.fgColor + "]%}"
-        + s.content
-        + "%{$reset_color%}"
-        + "%{$bg[" + s.separator.bgColor + "]%}"
-        + "%{$fg[" + s.separator.fgColor + "]%}"
-        + s.separator.content
-        + "%{$reset_color%}")
+      "%{$bg[" + s.bgColor + "]%}" + "%{$fg_bold[" + s.fgColor + "]%}" + s.content + "%{$reset_color%}"
+        + "%{$bg[" + s.separator.bgColor + "]%}" + "%{$fg[" + s.separator.fgColor + "]%}" + s.separator.content + "%{$reset_color%}")
       .mkString("") + "\r\n%% ")
   }
 
