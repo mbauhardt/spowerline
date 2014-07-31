c09675f657750632624dcaea1a0a93c4b170f1cc
Marko Bauhardt <marko.bauhardt@googlemail.com>
2014-07-31 18:12:58 +0200

    write method to add separators before every segment

--- src/main/scala/com/github/mbauhardt/spowerline/SPowerline.scala
+++ src/main/scala/com/github/mbauhardt/spowerline/SPowerline.scala
@@ -23,25 +23,44 @@ object Util {
 
 object Common {
   val emptySegment: Segment = Segment("")
-  val lastExitStatusSegment: Segment = Segment("%(?..%?)", "red", "blue", SegmentSeparator(Util.toExecutable(Util.segmentSeparatorContent), "blue", "blue"))
-  val timeSegment: Segment = Segment("%D{%a %d-%b}%@", "black", "blue", SegmentSeparator(Util.toExecutable(Util.segmentSeparatorContent), "blue", "white"))
-  val hostSegment: Segment = Segment("%n@%M", "black", "white", SegmentSeparator(Util.toExecutable(Util.segmentSeparatorContent), "white", "cyan"))
-  val pwdSegment: Segment = Segment("%~", "black", "blue", SegmentSeparator(Util.toExecutable(Util.segmentSeparatorContent), "blue", "green"))
+  val lastExitStatusSegment: Segment = Segment("%(?..%?)", "red", "blue")
+  val timeSegment: Segment = Segment("%D{%a %d-%b}%@", "black", "blue")
+  val hostSegment: Segment = Segment("%n@%M", "black", "white")
+  val pwdSegment: Segment = Segment("%~", "black", "blue")
 }
 
 object Vcs {
   def isInsideGitDirectory: String = "git rev-parse --is-inside-work-tree &> /dev/null";
-  val gitSegment: Segment = Segment("`git_prompt_info`", "black", "green", SegmentSeparator(Util.toExecutable(Util.combineCommands(Seq(isInsideGitDirectory, Util.segmentSeparatorContent))), "green", "black"))
+  val gitSegment: Segment = Segment(Util.toExecutable(Util.combineCommands(Seq(isInsideGitDirectory, "git_prompt_info"))), "black", "green")
 }
 
 object SPowerline extends App {
 
+
+  def createSeparators(segments: List[Segment]): List[(Segment, SegmentSeparator)] = {
+    val z: List[(Segment, SegmentSeparator)] = Nil
+    segments.foldRight(z) {
+      (segment, acc) =>
+        val bgColor = acc match {
+          case Nil => "black"
+          case a :: rest => a._1.bgColor
+        }
+        val separator: SegmentSeparator = SegmentSeparator(Util.toExecutable(Util.segmentSeparatorContent), segment.bgColor, bgColor)
+        (segment, separator) :: acc
+    }
+  }
+
+
   def zshString(segment: Segment): String = "%{$bg[" + segment.bgColor + "]%}" + "%{$fg_bold[" + segment.fgColor + "]%}" + segment.content + "%{$reset_color%}";
+
 
   def zshString(separator: SegmentSeparator): String = "%{$bg[" + separator.bgColor + "]%}" + "%{$fg[" + separator.fgColor + "]%}" + separator.content + "%{$reset_color%}";
 
   def renderPowerline(powerline: Powerline) = {
-    println("\r\n" + powerline.segments.map(s => zshString(s) + zshString(s.separator)).mkString + "\r\n%% ")
+
+    val segmentsWithSeparators = createSeparators(powerline.segments.toList)
+
+    println("\r\n" + segmentsWithSeparators.map(s => zshString(s._1) + zshString(s._2)).mkString + "\r\n%% ")
   }
 
   renderPowerline(Powerline(Seq(Common.lastExitStatusSegment, Common.pwdSegment, Vcs.gitSegment)))
--- src/main/scala/com/github/mbauhardt/spowerline/package.scala
+++ src/main/scala/com/github/mbauhardt/spowerline/package.scala
@@ -4,7 +4,7 @@ package object spowerline {
 
   case class Powerline(segments: Seq[Segment])
 
-  case class Segment(content: String, fgColor: String = null, bgColor: String = null, separator: SegmentSeparator = null)
+  case class Segment(content: String, fgColor: String = null, bgColor: String = null)
 
   case class SegmentSeparator(content: String, fgColor: String = null, bgColor: String = null)
 
