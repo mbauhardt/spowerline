a1d5baae2a975e792e0c3ef314d07a126a19971e
Marko Bauhardt <marko.bauhardt@googlemail.com>
2014-10-21 21:34:01 +0200

    implement generate fg and bg color functions

--- src/main/scala/com/github/mbauhardt/spowerline/PowerlineGenerator.scala
+++ src/main/scala/com/github/mbauhardt/spowerline/PowerlineGenerator.scala
@@ -7,9 +7,26 @@ object PowerlineGenerator {
     var acc = s
     val group = pe.element._1.group.toUpperCase
     val id = pe.element._1.id.toUpperCase
-    acc += (s"\nexport SPOWERLINE_$group" + "_" + s"$id=" + pe.element._1.content)
+    acc += (s"\nexport SPOWERLINE_$group" + "_" + s"$id" + "_CONTENT=" + pe.element._1.content)
     acc
   }
 
+  val generateForeground: (String, PowerlineElement) => String = (s, pe) => {
+    var acc = s
+    val group = pe.element._1.group.toUpperCase
+    val id = pe.element._1.id.toUpperCase
+    val fgcolor = pe.element._1.fg
+    acc += (s"\nexport SPOWERLINE_$group" + "_" + s"$id" + "_FG=" + s"%{$$fg[$fgcolor]%}")
+    acc
+  }
+
+  val generateBackground: (String, PowerlineElement) => String = (s, pe) => {
+    var acc = s
+    val group = pe.element._1.group.toUpperCase
+    val id = pe.element._1.id.toUpperCase
+    val bgcolor = pe.element._1.bg
+    acc += (s"\nexport SPOWERLINE_$group" + "_" + s"$id" + "_BG=" + s"%{$$bg[$bgcolor]%}")
+    acc
+  }
 
 }
--- src/test/scala/com/github/mbauhardt/spowerline/PowerlineGeneratorSuite.scala
+++ src/test/scala/com/github/mbauhardt/spowerline/PowerlineGeneratorSuite.scala
@@ -10,6 +10,20 @@ class PowerlineGeneratorSuite extends FunSuite {
     val pe = Empty.inc(Segment("common", "dir", "hello")).inc(Segment("vcs", "git", "world"))
     val pl = Powerline(pe)
     val s = pl.foldLeft("")(PowerlineGenerator.generateContent)
-    assert(s == "\nexport SPOWERLINE_COMMON_DIR=hello\nexport SPOWERLINE_VCS_GIT=world")
+    assert(s == "\nexport SPOWERLINE_COMMON_DIR_CONTENT=hello\nexport SPOWERLINE_VCS_GIT_CONTENT=world")
+  }
+
+  test("generateFgColor") {
+    val pe = Empty.inc(Segment("common", "dir", "hello", "red")).inc(Segment("vcs", "git", "world", "blue"))
+    val pl = Powerline(pe)
+    val s = pl.foldLeft("")(PowerlineGenerator.generateForeground)
+    assert(s == "\nexport SPOWERLINE_COMMON_DIR_FG=%{$fg[red]%}\nexport SPOWERLINE_VCS_GIT_FG=%{$fg[blue]%}")
+  }
+
+  test("generateBgColor") {
+    val pe = Empty.inc(Segment("common", "dir", "hello", bg = "cyan")).inc(Segment("vcs", "git", "world", bg = "blue"))
+    val pl = Powerline(pe)
+    val s = pl.foldLeft("")(PowerlineGenerator.generateBackground)
+    assert(s == "\nexport SPOWERLINE_COMMON_DIR_BG=%{$bg[cyan]%}\nexport SPOWERLINE_VCS_GIT_BG=%{$bg[blue]%}")
   }
 }
